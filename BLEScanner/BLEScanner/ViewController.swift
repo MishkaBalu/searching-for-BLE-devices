61d235e9ed98e87c96434e04aa2cf557a87a0d90
Leonid <duolts1997@gmail.com>
2020-02-05 13:05:00 +0200

    create project

---
+++ BLEScanner/BLEScanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BLEScanner
+//
+//  Created by Administrator on 05.02.2020.
+//  Copyright © 2020 Administrator. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
